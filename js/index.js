e971305b048514480a37e1096ac6c24a6ac92e10
kim-hansol99 <khs990920@naver.com>
2022-06-15 16:33:06 +0900

    06-15

--- js/index.js
+++ js/index.js
@@ -6,10 +6,75 @@ window.onload = function() {
             "s linear " +
           (Math.random() * 2 + 2) +
             "s infinite; top: " +
-          Math.random() * $("html").height() +
+          Math.random() * $("body").height() +
             "px; left: " +
-          Math.random() * $("html").width() +
+          Math.random() * $("body").width() +
             'px;"></div>';
         $("body").append(star);
         }
+
+        var words = ['UNIVERSE!','YOU!!',''],
+            currentStep = 0,
+            textEl = document.querySelector('.change-text'),
+            oldWord = '';
+        
+        setTimeout(changeWord, 1000);
+        
+        function changeWord() {
+          oldWord = textEl.innerHTML;
+        
+          if (oldWord.length < 1) {
+        
+            if (currentStep !== words.length -1) {
+                  currentStep ++;
+            }else {
+              currentStep = 0;
+            }
+            addNextWord();
+          } else {
+            setTimeout(deleteWord, 800);
+          }
+          
+        };
+        
+        function deleteWord() {
+          var WordLength = oldWord.length,
+              currentWord = textEl.innerHTML,
+              currentLength = currentWord.length;
+          
+        
+          // The word is deleted so, start adding in the new one
+          if (currentLength < 1) {
+            changeWord();
+            return;
+          }
+          
+          // Remove a charachter
+          textEl.innerHTML = currentWord.substring(0, currentLength - 1);
+          
+          setTimeout(deleteWord, 200);
+        }
+        
+        function addNextWord() {
+          var currentWord = textEl.innerHTML,
+              currentLength = currentWord.length,
+              nextWord = words[currentStep],
+              nextWordLength = nextWord.length;
+            
+          
+          if (currentLength === nextWordLength) {
+            changeWord();
+            return;
+          }
+          
+          // add a charachter
+          textEl.innerHTML = nextWord.substring(0, currentLength + 1);
+            
+          setTimeout(addNextWord, 200);
+}
+
+
+
+  
+  
 }
