cd5ca2bc0266c280c20ff5a282eb50889fbdd8d7
kim-hansol99 <khs990920@naver.com>
2022-07-16 22:08:42 +0900

    07-16

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  //배경
   for (var i = 0; i < 500; i++) {
     var star =
         '<div class="star" style="animation: twinkle ' +
@@ -13,6 +14,7 @@ window.onload = function() {
     $("body").append(star);
   }
 
+  //이미지 호버 이벤트
   $(".hello_img").on({
     mouseover:function(){
       $(this).find("img:nth-child(1)").stop().animate({opacity:0},600);
@@ -23,6 +25,7 @@ window.onload = function() {
     } 
   });
 
+  //변하는 텍스트
   var words = ['UNIVERSE!','YOU!'],
       currentStep = 0,
       textEl = document.querySelector('.change-text'),
@@ -79,26 +82,35 @@ window.onload = function() {
     setTimeout(addNextWord, 200);
   }
 
+  //스킬 클릭 이벤트
   const tabList = document.querySelectorAll('.circle-menu .list li');
   const contents = document.querySelectorAll('.circle-menu .cont_area .cont')
-  let activeCont = '';
+  let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+  console.log(tabList);
+  console.log(contents);
 
   for(var i = 0; i < tabList.length; i++){
     tabList[i].querySelector('.btn').addEventListener('click', function(e){
       e.preventDefault();
       for(var j = 0; j < tabList.length; j++){
+        // 나머지 버튼 클래스 제거
         tabList[j].classList.remove('is_on');
 
+        // 나머지 컨텐츠 display:none 처리
         contents[j].style.display = 'none';
       }
 
+      // 버튼 관련 이벤트
       this.parentNode.classList.add('is_on');
 
+      // 버튼 클릭시 컨텐츠 전환
       activeCont = this.getAttribute('href');
       document.querySelector(activeCont).style.display = 'block';
     });
   }
+  
 
+  ///모달
   $('[data-modal]').click(function(e){
     e.preventDefault();
     $(this).parent().find('[data-wrapper]').addClass('active');
