2a030aaeb7e0b23d4058c4400a0ba9905ffed68a
kim-hansol99 <khs990920@naver.com>
2022-07-18 17:39:11 +0900

    07-18

--- js/index.js
+++ js/index.js
@@ -84,8 +84,8 @@ window.onload = function() {
 
   //스킬 클릭 이벤트
   const tabList = document.querySelectorAll('.circle-menu .list li');
-  const contents = document.querySelectorAll('.circle-menu .cont_area .cont')
-  let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+  const contents = document.querySelectorAll('.cont_area .cont')
+  let activeCont = '';
   console.log(tabList);
   console.log(contents);
 
@@ -93,18 +93,15 @@ window.onload = function() {
     tabList[i].querySelector('.btn').addEventListener('click', function(e){
       e.preventDefault();
       for(var j = 0; j < tabList.length; j++){
-        // 나머지 버튼 클래스 제거
         tabList[j].classList.remove('is_on');
 
-        // 나머지 컨텐츠 display:none 처리
         contents[j].style.display = 'none';
       }
 
-      // 버튼 관련 이벤트
       this.parentNode.classList.add('is_on');
 
-      // 버튼 클릭시 컨텐츠 전환
       activeCont = this.getAttribute('href');
+      console.log(activeCont);
       document.querySelector(activeCont).style.display = 'block';
     });
   }
@@ -114,13 +111,14 @@ window.onload = function() {
   $('[data-modal]').click(function(e){
     e.preventDefault();
     $(this).parent().find('[data-wrapper]').addClass('active');
-    $('body').css("overflow", "hidden")
+    $('body').css("overflow", "hidden");
   });
   
   $('[data-wrapper], .close').click(
     function(modal){
       if (modal.target == this) {
         $('[data-wrapper]').removeClass('.modal-wrapper active');
+        $('body').css("overflow-Y", "scroll");
       }
     });
 }
